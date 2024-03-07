28fe41c2bba2302eb69a40fa6dab8f9e0dfe7e09
Al Simpson <73041922+AlfredSimpson@users.noreply.github.com>
2024-03-07 17:26:16 -0500

    Create youdontevengohere.sh

---
+++ youdontevengohere.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the user's username
+USER=$(whoami)
+
+# Get the current date and time
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+# Log file location - somewhere that it can be written to.
+LOGFILE="/var/log/alocationofalogfile.log"
+
+# This will append every command the user types to the log file
+# Format: user: command (date and time)
+trap 'echo "${USER}: $(history 1 | sed "s/^[ ]*[0-9]\+[ ]*//") (${TIMESTAMP})" >> ${LOGFILE}' DEBUG
