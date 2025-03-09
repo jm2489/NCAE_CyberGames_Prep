59b90072fd3619458c9f943dfcf75653d2131f82
Noah Jacobson <noah@noahjacobson.com>
2025-03-09 15:16:10 -0400

    fix(ProxmoxPaste): delay not working, right click problems
    * Fix delay logic.
    * Switched to button instead of right click.

--- ProxmoxPaste.js
+++ ProxmoxPaste.js
@@ -1,41 +1,66 @@
 (function () {
-    const delay = 1;
-    sendString = function(text) {
-        var el = document.querySelector("iframe").contentDocument.getElementById("canvas-id");
-        text.split("").forEach(x=>{
-            setTimeout(()=>{
-                 var needs_shift = x.match(/[A-Z!@#$%^&*()_+{}:\"<>?~|]/);
-                 let evt;
-                 if (needs_shift) {
-
-                     evt = new KeyboardEvent("keydown", {keyCode: 16});
-                     el.dispatchEvent(evt);
-                     evt = new KeyboardEvent("keydown", {key: x, shiftKey: true});
-                     el.dispatchEvent(evt);
-                     evt = new KeyboardEvent("keyup", {keyCode: 16});
-                     el.dispatchEvent(evt);
-
-                    }else if (x == '\n'){
-                        evt = new KeyboardEvent("keydown", {keyCode: 13});
-                    }else{
-                        evt = new KeyboardEvent("keydown", {key: x});
-                }
-                el.dispatchEvent(evt);
-            }, delay);
-        });
-
+    let delay = 20;
+    sendString = async (text, iframe) => {
+        if (!confirm('Paste ' +  text.split(/\n/).length + ' lines?'))
+            return;
+        const promptResult = prompt('Delay (in ms)', delay)
+        if (!promptResult || isNaN(promptResult)) {
+            return;
+        } else {
+            delay = parseInt(promptResult);
+        }
+        const canvas = iframe.contentDocument.getElementById('canvas-id');
+        const stopButton = document.getElementById('stop-button');
+        let stop = false;
+        const setStop = () => {
+            stop = true;
+        };
+        stopButton.addEventListener('click', setStop);
+        stopButton.removeAttribute('disabled');
+        canvas.focus();
+        for (const x of text.split('')) {
+            if (stop)
+                break;
+            await new Promise(resolve => setTimeout(resolve, delay));
+            var needs_shift = x.match(/[A-Z!@#$%^&*()_+{}:\"<>?~|]/);
+            let evt;
+            if (needs_shift) {
+                evt = new KeyboardEvent('keydown', {keyCode: 16});
+                canvas.dispatchEvent(evt);
+                evt = new KeyboardEvent('keydown', {key: x, shiftKey: true});
+                canvas.dispatchEvent(evt);
+                evt = new KeyboardEvent('keyup', {keyCode: 16});
+               }else if (x == '\n'){
+                   evt = new KeyboardEvent('keydown', {keyCode: 13});
+                   canvas.dispatchEvent(evt);
+                   await new Promise(resolve => setTimeout(resolve, delay));
+                   evt = new KeyboardEvent('keyup', {keyCode: 13});
+               }else{
+                   evt = new KeyboardEvent('keydown', {key: x});
+           }
+           canvas.dispatchEvent(evt);
+        }
+        stopButton.removeEventListener('click', setStop);
+        stopButton.setAttribute('disabled', true);
     };
     setTimeout(()=>{
-            console.log("Starting up noVNC Copy/Paste (for Proxmox)");
-
-            document.querySelector("iframe").contentDocument.querySelector("canvas").setAttribute("id", "canvas-id");
-
-            document.querySelector("iframe").contentDocument.querySelector("canvas").addEventListener("mousedown", (e)=>{
-                if(e.button == 2){ // Right Click
-                    navigator.clipboard.readText().then(text =>{
-                        window.sendString(text);
-                    });
-                }
+            if (document.getElementById('paste-button') != null)
+                return;
+            const iframe = document.querySelector('iframe');
+            const button = document.createElement('button');
+            const stopButton = document.createElement('button');
+            button.setAttribute('id', 'paste-button');
+            stopButton.setAttribute('id', 'stop-button');
+            stopButton.setAttribute('disabled', true);
+            button.innerText = 'Paste-inator';
+            stopButton.innerText = 'Stop Paste';
+            button.addEventListener('click', () => {
+                navigator.clipboard.readText().then(text =>{
+                    window.sendString(text, iframe);
+                });
             });
-    }, 1000);
+            iframe.parentElement.prepend(stopButton);
+            iframe.parentElement.prepend(button);
+            iframe.contentDocument.querySelector('canvas').setAttribute('id', 'canvas-id');
+    }, 500);
 })()
